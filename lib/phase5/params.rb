3fc6ffb9dcf3d38777b9caa62385a8abc7c0e381
chris <chris@chriss-MacBook-Pro.local>
2015-05-20 11:08:28 -0700

    Working on phase 5

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -2,22 +2,12 @@ require 'uri'
 
 module Phase5
   class Params
-    # use your initialize to merge params from
-    # 1. query string
-    # 2. post body
-    # 3. route params
-    #
-    # You haven't done routing yet; but assume route params will be
-    # passed in as a hash to `Params.new` as below:
-
-    # INSTRUCTIONS
-
     def initialize(req, route_params = {})
+      # TODO: find method that accesses request body of @req
+      @request_body = req.query_string
       @params = {}
     end
-    # NB: Your Params#[] getter method should respond indifferently to both symbol and string versions of its keys. In other words, params[:id] and params['id'] should both work.
-    #
-    # Instantiate a Params object in your controller, as an ivar. This will keep track of the params for the controller.
+
     def [](key)
       key = key.to_s
       @params[key]
@@ -29,22 +19,44 @@ module Phase5
 
     class AttributeNotFoundError < ArgumentError; end;
 
-    private
-    # this should return deeply nested hash
-    # argument format
-    # user[address][street]=main&user[address][zip]=89436
-    # should return
-    # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
-    def parse_www_encoded_form(www_encoded_form)
+    # private
+    def self.parse_www_encoded_form(www_encoded_form)
+      @params = {}
       params_from_url = URI::decode_www_form(www_encoded_form) # array of keys and values
-      params_from_url.each do |pair|
-        @params_from_url[pair.first] = pair.last
+      # 'user[house][name]=Disneyland' = > [['user[house][name]', 'Disneyland']]
+      params_from_url.each do |key_value_pair|
+        keys = key_value_pair.first
+        de_nested_keys = self.parse_key(keys)
+        val = key_value_pair.last
+        @params = self.nest(de_nested_keys, val)
       end
+      @params
+      #
+      #
+      #
+      # params_from_url.each do |pair|
+      #   @params[pair.first] = pair.last
+      # end
+      # @params
+      # # RESULT
+      # 'user=mike' => [['user', 'mike']]
+      # 'user[house][name]' => [['user[house][name]', 'Disneyland']]
+      # @req.body
     end
 
-    # this should return an array
-    # user[address][street] should return ['user', 'address', 'street']
-    def parse_key(key)
+    def self.nest(keys, val)
+      if keys.length == 1
+        return {keys.first => val}
+      end
+      {keys.first => self.nest(keys.drop(1), val)}
+    end
+
+    def self.parse_key(keys)
+      if keys.include?('[') or keys.include?(']')
+        keys = keys.split(/\]\[|\[|\]/)
+      else
+        [keys]
+      end
     end
   end
 end
