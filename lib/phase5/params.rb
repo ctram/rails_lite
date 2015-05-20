f0601bb72b3ca4341151a2ff82cb21a581a6d555
chris <chris@chriss-MacBook-Pro.local>
2015-05-20 14:38:36 -0700

    Finish Phase 5

--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -5,25 +5,22 @@ module Phase5
   class Params
     def initialize(req, route_params = {})
       byebug
-      request_query_string = req.query_string
-      request_body = req.body
+      query_string = req.query_string
+      body = req.body
+      byebug
 
-      unless request_query_string.nil? or request_query_string.include?('[') or request_query_string.include?(']')
-        key = request_query_string.split('=').first
-        val = request_query_string.split('=').last
-        @params = {key => val}
-        return
-      end
+      query_string_hash = parse_www_encoded_form(query_string) unless query_string.nil?
+      body_hash = parse_www_encoded_form(body) unless body.nil?
 
-      hash_params_query_string = build_params(request_query_string) unless request_query_string.nil?
-      hash_params_body = build_params(request_body) unless request_body.nil?
 
-      if hash_params_body.nil?
-        @pararms = hash_params_query_string
-      elsif hash_params_query_string.nil?
-        @params = hash_params_body
+      if query_string.nil? and body.nil?
+        @params = {}
+      elsif query_string.nil?
+        @params = body_hash
+      elsif body.nil?
+        @params = query_string_hash
       else
-        @params = hash_params_body.merge(hash_params_query_string) do |key, oldval, newval|
+        @params = body_hash.merge(query_string_hash) do |key, oldval, newval|
           if !newval.nil?
             {oldval.keys.first => oldval.values.first, newval.keys.first => newval.values.first}
           else
@@ -31,6 +28,8 @@ module Phase5
           end
         end
       end
+      byebug
+      @params.merge!(route_params)
     end
 =begin
   h11.merge(h22) do |key, oldval, newval|
@@ -58,50 +57,51 @@ module Phase5
 
     # private
 
-    def parse_www_encoded_form(www_encoded_form)
-
+    def parse_www_encoded_form(www_encoded_form) # parameter is a url address
       params = {}
+      after_question_mark = www_encoded_form.split('?').last
+      key_value_pairs = URI::decode_www_form(after_question_mark) # array of keys and values
 
-      params_from_url = URI::decode_www_form(www_encoded_form) # array of keys and values
-      #  RESULT =>
-        # [["user[house][name]", "Disneyland"], ["user[house][address]", "haight"]]
-      self.build_params(params_from_url)
+      # RESULT
+      # 'http://www.google.com/?name=mike&height=11'
+      #   =>
+      # [["name", "mike"], ["height", "11"]]
 
-      # build_params(params_from_url)
+      # RESULT with nested keys
+      # 'http://www.google.com/?user[boss][name]=mike&user[boss][height]=11'
+      #   =>
+      # [["user[boss][name]", "mike"], ["user[boss][height]", "11"]]
+
+      build_params(key_value_pairs)
+
+
+
     end
 
     def build_params(data)
-
+      # unless data.include?('[')
+      #   key = data.split('=').first
+      #   val = data.split('=').last
+      #   return
+      # end
       params = {}
-
       data.each do |pair|
-
         keys = parse_key(pair.first) # array
-
         val = pair.last
-
         current = params
-
         keys.each.with_index do |key, idx|
-
           if idx == keys.length - 1
-
             current[key] = val
-
           else
-
             current[key] ||= {}
-
             current = current[key]
-
           end
         end
       end
-
       params
     end
 
-    def nest(keys, val)
+    def self.nest(keys, val)
       if keys.length == 1
         return {keys.first => val}
       end
